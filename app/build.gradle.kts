d8d900564f50596068c4f868ff32e0f2e86b70b8
thinhlh <thinhlhhe170034@fpt.edu.vn>
2024-10-04 14:16:14 +0700

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,6 @@ android {
 
 dependencies {
     implementation("com.google.android.material:material:1.9.0")
-
     implementation("com.intuit.sdp:sdp-android:1.1.1")
     implementation("com.intuit.ssp:ssp-android:1.1.1")
     implementation("net.sourceforge.jtds:jtds:1.3.1")
@@ -47,6 +46,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity:1.8.0")
+    implementation("com.google.android.ads:mediation-test-suite:3.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
---
+++ app/src/main/java/com/example/prm_project/Adapter/BestSellerAdapter.java
@@ -0,0 +1,35 @@
+//package com.example.prm_project.Adapter;
+//
+//import android.content.Context;
+//import android.view.ViewGroup;
+//
+//import androidx.annotation.NonNull;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//import java.util.ArrayList;
+//
+//
+//public class BestSellerAdapter extends RecyclerView.Adapter<BestSellerAdapter.ItemViewHolder> {
+//    public BestSellerAdapter(Context callback, ArrayList<Item> listItem) {
+//        this.mContext = callback;
+//        this.mListItem = listItem;
+//    }
+//    @NonNull
+//    @Override
+//    public BestSellerAdapter.ItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        return null;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull BestSellerAdapter.ItemViewHolder holder, int position) {
+//
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        return 0;
+//    }
+//
+//    public class ItemViewHolder extends RecyclerView.ViewHolder {
+//    }
+//}
